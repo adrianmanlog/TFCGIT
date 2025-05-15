5858837c5ca14d8bb384d4c0aad2a8ab198da3a9
Tanjx <adriangalileaa@gmail.com>
2025-05-15 17:31:35 +0200

    pruebas

---
+++ TFCAdrianGalilea/TestTFC/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Manejo.Manejadores.Controladores;
+using Manejo.Manejadores;
+using Manejo.EntidadesDTO;
+namespace TestTFC
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClienteDTO cliente = new ClienteDTO("14778945G","pruebas","unittes","pruebas@gmail.com","458798564","avenida burgos","26007","16636730W");
+
+            // Arrange
+            var controlador = new ClienteController();
+
+            // Act
+            var resultado = controlador.InsertarCliente(cliente);
+
+            // Assert
+            Assert.AreEqual(true, resultado);
+        }
+    }
+}
